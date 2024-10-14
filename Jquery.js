3614ece7fff50ebaa80cbb5207b10db8d89bc0c7
Mic-1967 <mwill017@gmail.com>
2024-10-14 12:00:40 -0400

    version 1

--- Jquery.js
+++
@@ -1,5 +0,0 @@
-#area{
-    width: 200px;
-    height: 200px;
-    background-color: #953674;
-}
---
+++ script.js
@@ -0,0 +1,9 @@
+function alertHover() {
+    $("#area").css("background-color", "green");
+    alert("close me to turn the box green.");
+}
+
+function reset() {
+    $("#area").css("background-color","#953674");
+            
+}
